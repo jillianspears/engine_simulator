f3e1124a2a78900ffa193bb3e68640d4037b747e
JillianSpears <jmaries@bu.edu>
2026-05-24 14:32:40 -0700

    init

---
+++ constants and conversions/constants.h
@@ -0,0 +1,6 @@
+/**
+ * Universal Constants with units and some notes
+ */
+
+extern const double kUniversalGasConstantFtLbfLbmolR = 1545.4;
+extern const double kSeaLevelPressurePsi = 14.7;
---
+++ parameters/engine_parameters.h
@@ -0,0 +1,14 @@
+/**
+ * Parameters and Design Constraints for a Solid or Liquid Rocket Engine
+ */
+
+namespace liquid_engine_params {
+
+double thrust = 0.00;
+double chamber_pressure_psi = 0.00; 
+double oxygen_fuel_ratio = 0.00;
+double exit_pressure_psi = 0.00;
+double burn_time = 0.00;
+double specific_heat_ratio = 0.00;
+
+} // liquid_engine_params
